bc8f5f6a4aac8774ffdefc542eda7d6287e68548
Josh Hawes <joshua.m.hawes@gmail.com>
2024-02-11 19:16:58 +0000

    connecting database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-hawesjm-cifsfdjango-16hclqgpjkt.ws-eu108.gitpod.io']
 
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Application definition
 
@@ -74,13 +78,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# # DATABASES = {
+   # #  'default': {
+        # # 'ENGINE': 'django.db.backends.sqlite3',
+        # # 'NAME': BASE_DIR / 'db.sqlite3',
+    # # }
+# # }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
